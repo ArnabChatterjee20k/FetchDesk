b2579a7135e226d34172192eac506fb5299f1e35
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2025-06-01 23:41:26 +0530

    added tweening between two state in the avatar

--- frontend/src/lib/components/account/Account.svelte
+++ frontend/src/lib/components/account/Account.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { Tween, tweened } from "svelte/motion";
+  import { cubicOut } from "svelte/easing";
   import { getSidebarContext } from "../sidebar/context";
   import AvatarFallback from "../ui/avatar/avatar-fallback.svelte";
   import AvatarImage from "../ui/avatar/avatar-image.svelte";
@@ -7,20 +9,37 @@
   export let accountName: string;
   export let imageURL: string = "";
   export let avatarLetter: string = "";
+
   const avatar = avatarLetter
     ? avatarLetter
     : accountName.charAt(0).toUpperCase();
+
   const store = getSidebarContext();
+
+  const borderRadius = new Tween(50, {
+    duration: 100,
+    easing: cubicOut,
+  });
+
+  $: borderRadius.set($store.open ? 50 : 16);
 </script>
 
 <div class="flex items-center gap-2">
-  <Avatar>
-    <AvatarFallback class={`transition-all duration-300 ease-in-out`}
-      >{avatar}</AvatarFallback
+  <Avatar
+    class="transition-all"
+    style="border-radius: {borderRadius.current}%;"
+  >
+    <AvatarFallback
+      style="border-radius: {borderRadius.current}%;"
+      class={`w-full h-full flex items-center justify-center`}
     >
+      {avatar}
+    </AvatarFallback>
+
     {#if imageURL}
       <AvatarImage>{imageURL}</AvatarImage>
     {/if}
   </Avatar>
+
   <span class="font-medium text-md">{accountName}</span>
 </div>
